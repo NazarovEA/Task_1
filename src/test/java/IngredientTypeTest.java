12f04cc71fa5c0a56d3e6c51e5f4ea38f013bb21
Евгений <nk-nazarovEA@yandex.ru>
2026-03-01 22:45:20 +0300

    тесты классаIngredientTypeTest

--- src/test/java/IngredientTypeTest.java
+++ src/test/java/IngredientTypeTest.java
@@ -1,2 +1,19 @@
+import org.junit.jupiter.api.Test;
+import praktikum.IngredientType;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class IngredientTypeTest {
+    @Test
+    public void quantityType() {
+    assertEquals(2, IngredientType.values().length);
+    }
+    @Test
+    public void presentTypeSauce() {
+        assertEquals("SAUCE", IngredientType.SAUCE.name(), "Соусы должны быть в типах");
+    }
+    @Test
+    public void presentTypeFilling() {
+        assertEquals("FILLING", IngredientType.FILLING.name(), "Начинки должны быть в типах");
+    }
 }
